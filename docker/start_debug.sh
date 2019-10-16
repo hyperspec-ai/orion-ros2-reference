5607fe5f7ee0c8f6c66e7d7b267f495b49856a38
Marcus von Wilamowitz-Moellendorff <marcus.von_wilamowitz-moellendorff@daimler.com>
2019-10-16 16:47:51 +0200

    Added path as single cli parameter

--- docker/start_debug.sh
+++ docker/start_debug.sh
@@ -1,2 +1,2 @@
 echo "ConfigNode Directory mapped to $WS_KROS" 
-docker run --net orion_nw --ip 172.18.0.10 -it -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev gdbserver :1234 install/config/lib/config/ConfigNode_exec
+docker run --net orion_nw --ip 172.18.0.10 -it -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev gdbserver :1234 "install/config/lib/config/ConfigNode_exec ./config"
--- src/config_node/exec_entry.cpp
+++ src/config_node/exec_entry.cpp
@@ -7,13 +7,21 @@
 
 int main(int argc, char* argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::NodeOptions options;
-    options.allow_undeclared_parameters(true);
-    options.automatically_declare_parameters_from_overrides(true);
-    options.use_intra_process_comms(true);
-    options.append_parameter_override("path", "./config");
-    rclcpp::spin(std::make_shared<config::ConfigNode>(options));
-    rclcpp::shutdown();
+    //TODO: make explicit parsing for path as this can also be used for rclcpp parameters
+    if(argc == 2)
+    {
+        rclcpp::init(argc, argv);
+        rclcpp::NodeOptions options;
+        options.allow_undeclared_parameters(true);
+        options.automatically_declare_parameters_from_overrides(true);
+        options.use_intra_process_comms(true);
+        options.append_parameter_override("path", argv[1]);
+        rclcpp::spin(std::make_shared<config::ConfigNode>(options));
+        rclcpp::shutdown();
+    }
+    else
+    {
+        printf("Wrong number of parameters");
+    }
     return 0;
 }
