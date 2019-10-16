33997dd4abdcab3c57c9b6064f26596bbcce956c
Marcus von Wilamowitz-Moellendorff <marcus.von_wilamowitz-moellendorff@daimler.com>
2019-10-16 17:39:23 +0200

    Debug script adapted to deploy automoatically to smulated infra and reset of repository

--- docker/start_debug.sh
+++ docker/start_debug.sh
@@ -1,2 +1,5 @@
-echo "ConfigNode Directory mapped to $WS_KROS" 
+echo "ConfigNode Directory mapped to $WS_KROS"
+pwd
+bash deploy_all_pc.sh
+cp config/repository/repository_empty.json config/repository/repository.json
 docker run --net orion_nw --ip 172.18.0.10 -it -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev gdbserver :1234 "install/config/lib/config/ConfigNode_exec ./config"
