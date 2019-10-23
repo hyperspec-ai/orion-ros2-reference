180d51a3a1e7c6654b80c27d2cd92e4bb6460f19
Martin Zielinski <darthcoder1@gmail.com>
2019-10-23 21:33:19 +0200

    Ensure the WS_NODE path is set when invoking `build.sh`
    As default value, we initialize WS_NODE=/opt/ws_kros which is the location
    within the container.

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# When invoked on jenkins, WS_NODE is not set, so we default set it to /opt/ws_kros
+if [ -z "$WS_NODE" ] then
+    WS_NODE=/opt/ws_kros
+fi
+
 docker run  -v $WS_NODE:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev \
     "colcon build" --cmake-args ' -DCMAKE_BUILD_TYPE=Debug' -DCMAKE_CXX_FLAGS="-Werror -Wall"
 
