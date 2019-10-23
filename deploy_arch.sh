e549886ecfa20ab498efff47ae58434cf0094e50
Martin Zielinski <martin.zielinski@daimler.com>
2019-10-23 16:00:18 +0200

    Added simple deploy script to prepare architecture installation directory
    The script compresses the .so files of each node and stores them in
    `install/<arch>/NodeName.tar.gz`.
    The script is currently hardcoded to `x64` only, but we'll extend it as soon as x-compile
    is set up and running.
    
    The `deploy_arch.sh` script is called from the `build.sh` and invoked in the context of the
    host machine.

---
+++ deploy_arch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This is a helper script that collects the nodes from it's install directory,
+# compresses them and stores them in the install directory for the specific
+# architecture
+ARCH=x64
+ARCH_INSTALL_PATH=./install/$ARCH
+
+# usage deploy_to_arch <path-to-lib> <tar-filename>
+function deploy_path_to_arch() {
+    tar --transform 's/.*\///g' -zcvf /tmp/$2 $1
+    cp /tmp/$2 $ARCH_INSTALL_PATH/$2
+    echo "Deploy succeeded: " $2
+}
+
+# usage deploy_to_arch <package-name> <node-name>
+function deploy_node_for_arch() {
+    tar --transform 's/.*\///g' -zcvf /tmp/$2.tar.gz ./install/$1/lib/lib$2.so
+    cp /tmp/$2.tar.gz $ARCH_INSTALL_PATH
+    echo "Deploy succeeded: " $2
+}
+
+mkdir -p $ARCH_INSTALL_PATH
+
+deploy_node_for_arch config ConfigNode
+deploy_node_for_arch rtcm RtcmNode
+deploy_node_for_arch ublox UbxGpsNode
+deploy_path_to_arch "./install/rtcm_msgs/lib/librtcm_msgs__rosidl_typesupport_cpp.so ./install/rtcm_msgs/lib/librtcm_msgs__rosidl_typesupport_fastrtps_cpp.so ./install/rtcm_msgs/lib/librtcm_msgs__rosidl_typesupport_introspection_cpp.so" rtcm_msgs.tar.gz
--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,6 @@
-docker run  -v $WS_NODE:/opt/ws_kros --privileged -v /dev:/dev -v /opt/ws_kros:/opt/ws_kros --rm orion:dev "colcon build" --cmake-args ' -DCMAKE_BUILD_TYPE=Debug' -DCMAKE_CXX_FLAGS="-Werror -Wall"
+#!/bin/bash
+docker run  -v $WS_NODE:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev \
+    "colcon build" --cmake-args ' -DCMAKE_BUILD_TYPE=Debug' -DCMAKE_CXX_FLAGS="-Werror -Wall"
+
+# copy and compress binaries
+(cd $WS_NODE;./deploy_arch.sh)
