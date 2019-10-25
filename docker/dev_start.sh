7ea5ec5306ed8220b7689956309f95f264356b02
Martin Zielinski <darthcoder1@gmail.com>
2019-10-25 11:18:56 +0200

    Added argument to `dev_start.sh` that specifies how to start it
    Valid arguments are:
     * run: starts the docker and the config-node (runs the orion ros stack)
     * shell: starts the docker and opens the shell
     * debug: starts docker with config node and gdbserver

--- docker/dev_start.sh
+++ docker/dev_start.sh
@@ -1,1 +1,26 @@
-docker run --net orion_nw --ip 172.18.0.10 -it -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev
+#!/bin/bash
+
+# read start mode from arguments
+START_MODE=$1
+
+if [ -z "$START_MODE" ]; then
+    START_MODE=shell
+fi
+
+DOCKER_BASE_CMD="docker run --ip 172.18.0.10 -it -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev"
+ORION_START_CMD="install/config/lib/config/ConfigNode_exec ./config"
+
+case "$START_MODE" in
+    run)
+        $DOCKER_BASE_CMD $ORION_START_CMD
+        ;;
+    debug)
+        $DOCKER_BASE_CMD gdbserver :1234 $ORION_START_CMD
+        ;;
+    shell)
+        $DOCKER_BASE_CMD
+        ;;
+    *)
+        echo "Usage: $0 {run|debug|shell}".
+    exit 1
+esac
