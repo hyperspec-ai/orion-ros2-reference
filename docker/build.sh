2c3e98a3f6354d46e2c5b9d2a3e4f1c4299f3861
Martin Zielinski <darthcoder1@gmail.com>
2019-10-25 10:05:18 +0200

    Remove WS_NODE and use WS_KROS consistently

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# When invoked on jenkins, WS_NODE is not set, so we default set it to /opt/ws_kros
-if [ -z "$WS_NODE" ]; then
-    WS_NODE=/opt/ws_kros
+# When invoked on jenkins, WS_KROS is not set, so we default set it to /opt/ws_kros
+if [ -z "$WS_KROS" ]; then
+    WS_KROS=/opt/ws_kros
 fi
 
-docker run  -v $WS_NODE:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev \
+docker run  -v $WS_KROS:/opt/ws_kros --privileged -v /dev:/dev --rm orion:dev \
     "colcon build" --cmake-args ' -DCMAKE_BUILD_TYPE=Debug' -DCMAKE_CXX_FLAGS="-Werror -Wall"
 
 # copy and compress binaries
-(cd $WS_NODE;./deploy_arch.sh)
+(cd $WS_KROS;./deploy_arch.sh)
