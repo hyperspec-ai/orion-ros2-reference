2a3c05293a26f5584809e97af1aa5fac6852490b
Marcus von Wilamowitz-Moellendorff <marcus.von_wilamowitz-moellendorff@daimler.com>
2019-10-28 17:12:10 +0100

    added LD_LIBARARY_PATH extenstion

--- docker/start_debug_arm.sh
+++ docker/start_debug_arm.sh
@@ -4,5 +4,5 @@ cp config_arm/repository/repository_empty.json config_arm/repository/repository.
 #sshpass -p $ORION_XAVIER1_PASSWORD rsync --progress -avz  -e ssh $WS_KROS/install_arm/ $ORION_XAVIER1_USER@$ORION_XAVIER1_IP:/opt/orion
 sshpass -p $ORION_XAVIER1_PASSWORD rsync --progress -avz  -e ssh $WS_KROS/config_arm/ $ORION_XAVIER1_USER@$ORION_XAVIER1_IP:/opt/orion/config_arm
 sshpass -p $ORION_XAVIER1_PASSWORD ssh  -t $ORION_XAVIER1_USER@$ORION_XAVIER1_IP 'cd /opt/orion && export COLCON_CURRENT_PREFIX="/opt/orion" &&
-source /opt/orion/local_setup.sh && source /opt/ros2_ws/install/setup.bash && gdbserver :1234 /opt/orion/config/lib/config/ConfigNode_exec /opt/orion/config_arm'
+source /opt/orion/local_setup.sh && source /opt/ros2_ws/install/setup.bash && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/orion/lib_message && gdbserver :1234 /opt/orion/config/lib/config/ConfigNode_exec /opt/orion/config_arm'
 
